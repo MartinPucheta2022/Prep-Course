2df057421d16c63bc9cffef05ee69dc75bbebaa1
Martin Pucheta <martinpucheta44@gmail.com>
2022-10-04 05:24:21 -0300

    homeworkI.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -41,7 +41,6 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  var resta = x - y;
   return x - y;
 }
 
@@ -240,12 +239,12 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if(letra.length > 1){
-    return "Dato incorrecto"
+    return "Dato incorrecto";
   }
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
-    return "Es vocal"
+    return "Es vocal";
   }
-  return "Dato incorrecto"
+  return "Dato incorrecto";
 }
 
 
